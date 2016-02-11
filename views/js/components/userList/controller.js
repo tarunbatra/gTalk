113c56d8ee11f74b351e61c19260409b88f4f449
Tarun Batra <tarun.batra00@gmail.com>
2016-02-11 17:53:49 +0530

    okd

--- views/js/components/userList/controller.js
+++ views/js/components/userList/controller.js
@@ -19,38 +19,22 @@ userList.controller('userListController',['$scope','apiService','socketService',
 		console.log('notification');
 		$scope.$evalAsync(function()
 		{
-			var myAcc=_.findWhere(data.users,{username:me.username});
 			$scope.users= _.reject(data.users,{username:me.username});
 			_.each($scope.users,function(u)
 			{
-				if(u.username==data.cause)
-				{
-					if($scope.active.username!=data.cause)
-					{
-						u.notify=true;
+				if(u.username==data.cause) {
+					if ($scope.active.username != data.cause) {
+						u.notify = true;
 					}
-					if(data.code)
-					{
-						$scope.code=data.code;
+					if (data.code) {
+						$scope.code = data.code;
 					}
-					if($scope.active.username== u.username)
-					{
-						$scope.$evalAsync(function()
-						{
-							$scope.active=u;
+					if ($scope.active.username == u.username) {
+						$scope.$evalAsync(function () {
+							$scope.active = u;
 						});
 					}
 				}
-				var peer= _.findWhere(u.peers,{peerid:myAcc._id});
-				_.each(peer.messages,function(msg)
-				{
-					u.count=0;
-					if(!msg.read)
-					{
-						u.count++;
-					}
-				});
-
 			});
 		});
 	});
