75846fca87919dca1325de602ac3d9aa5a2144e8
Tarun Batra <tarun.batra00@gmail.com>
2016-02-15 18:10:47 +0530

    first test

--- tests/sample.js
+++
@@ -1,6 +0,0 @@
-angular.module('calculatorApp', []).controller('CalculatorController', function CalculatorController($scope) {
-  $scope.z=0;
-  $scope.sum = function() {
-    $scope.z = $scope.x + $scope.y;
-  };
-});
--- tests/tests.js
+++ tests/tests.js
@@ -1,24 +1,79 @@
-describe('appTest', function() {
+describe('signInTest', function() {
   beforeEach(module('app.core'));
-  beforeEach(module('app.components'));
-  beforeEach(module('app.sections'));
-  beforeEach(module('ngRoute'));
-  beforeEach(module('app'));
+  beforeEach(module('signIn'));
 
   var $controller;
+
+  module(function($provide) {
+    $provide.service('alertService', function() {
+      return {
+        show: function(msg) {
+          console.log(msg);
+        }
+      };
+    });
+  });
+  module(function($provide) {
+    $provide.factory('apiService', ['$q', function($q) {
+      function save(data) {
+        if (passPromise) {
+          return $q.when();
+        } else {
+          return $q.reject();
+        }
+      }
+      return {
+        signIn: save
+      };
+    }]);
+  });
 
 
   beforeEach(inject(function(_$controller_) {
     $controller = _$controller_;
   }));
 
-  describe('zIs1', function() {
-    it('z should be 1', function() {
+  describe('username should be 5 characters minimum', function() {
+    it('less than 5 should not work', function() {
       var $scope = {};
-      var controller = $controller('appController', {
+      var controller = $controller('signInController', {
         $scope: $scope
       });
-      expect($scope.z).toBe(1);
+      $scope.username = '1234';
+      $scope.login();
+      expect($scope.warning).toBe('Username should be 5 characters minimum');
+    });
+    it('greater than equal to 5 should work', function() {
+      var $scope = {};
+      var controller = $controller('signInController', {
+        $scope: $scope
+      });
+      $scope.username = '12345';
+      $scope.password='qqqqqqqqq';
+      $scope.login();
+      expect($scope.warning).toBe('');
+    });
+  });
+  describe('password should be 8 characters minimum', function() {
+    it('less than 8 should not work', function() {
+      var $scope = {};
+      var controller = $controller('signInController', {
+        $scope: $scope
+      });
+      $scope.username = '12345';
+      $scope.password='12345';
+      $scope.login();
+      expect($scope.warning).toBe('Password should be 8 characters minimum');
+    });
+    it('greater than 8 should work', function() {
+      var $scope = {};
+      var controller = $controller('signInController', {
+        $scope: $scope
+      });
+      $scope.username = '12345';
+      $scope.password='12345678';
+      $scope.login();
+      expect($scope.warning).toBe('');
     });
   });
 });
