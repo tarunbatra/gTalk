0db74e067895d4cd554ac5d3d480f23ff8829f38
Tarun Batra <tarun.batra00@gmail.com>
2016-02-15 13:23:10 +0530

    wrote first test

---
+++ tests/test.js
@@ -0,0 +1,28 @@
+describe('homeTest', function () {
+
+  beforeEach(module('home'));
+
+  var $controller;
+
+  beforeEach(inject(function(_$controller_){
+    $controller = _$controller_;
+  }));
+
+  describe('signed in', function () {
+    it('signed in user should have an entry in localstorage', function () {
+      var $scope = {};
+      var controller = $controller('homeController', { $scope: $scope });
+
+      expect($scope.me).toBeTruthy();
+    });
+
+    it('localstorage entry should be valid', function () {
+      var $scope = {};
+      var controller = $controller('homeController', { $scope: $scope });
+      expect($scope.me.username.toBeDefined());
+      expect($scope.me.token.toBeDefined());
+      expect($scope.me._id.toBeDefined());
+    });
+  });
+
+});
--- views/js/sections/home/controller.js
+++ views/js/sections/home/controller.js
@@ -1,13 +1,13 @@
 home.controller('homeController', ['$scope', 'apiService', 'socketService', 'checkSession', function($scope, user, socket) {
   try {
-    var me = JSON.parse(localStorage.getItem('data'));
-    user.auth(me, function(res) {
+    $scope.me = JSON.parse(localStorage.getItem('data'));
+    user.auth($scope.me, function(res) {
       if (!res.body.authenticated) {
         localStorage.removeItem('data');
         location.href = '#index';
       } else {
         user.getOne({
-          id: me.username
+          id: $scope.me.username
         }, function(res) {
           socket.emit('connection', res.body);
         });
