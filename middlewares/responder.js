9e5abdc9da3dae2019ba726db3da38c5a96e212f
Tarun Batra <tarun.batra00@gmail.com>
2016-02-19 17:50:35 +0530

    removed magic numbers

--- middlewares/responder.js
+++ middlewares/responder.js
@@ -1,7 +1,11 @@
 module.exports = function(req, res, err, data) {
+
+  var ERROR_STATUS=1200;
+  var SUCCESS_STATUS=1000;
+
   res.status(200).send({
     api: true,
-    code: err ? 1200 : 1000,
+    code: err ? ERROR_STATUS : SUCCESS_STATUS,
     body: err ? null : data,
     messages: err ? err.msg : [],
     meta: null
